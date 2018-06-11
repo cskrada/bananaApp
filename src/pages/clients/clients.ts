83aeae5af0191aca3317d0aaf30d9f72fa017759
cskrada <frada@overcom.net>
2018-06-11 10:11:27 -0400

    fue agregada la funcionalidad del boton de agregar cliente y de ver cliente en la pantalla de la lista general de los clientes

--- src/pages/clients/clients.ts
+++ src/pages/clients/clients.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+//import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { SeeclientPage } from '../seeclient/seeclient';
+import { AddclientPage } from '../addclient/addclient';
 /**
  * Generated class for the ClientsPage page.
  *
@@ -8,18 +10,134 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+//@IonicPage()
 @Component({
   selector: 'page-clients',
   templateUrl: 'clients.html',
 })
 export class ClientsPage {
+	items;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {
+  	this.initializeItems();
   }
+  initializeItems(){
+  	this.items = [
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	}
+    	];
+  }
+  	getItems(ev) {
+    	this.initializeItems();
+
+    	var val = ev.target.value;
+
+    	if (val && val.trim() != '') {
+      		this.items = this.items.filter((item) => {
+        	return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      		})
+    	}
+	}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ClientsPage');
+  }
+
+  seeClient(){
+  	this.navCtrl.push(SeeclientPage);
+  }
+
+  addClient(){
+  	this.navCtrl.push(AddclientPage);
   }
 
 }
