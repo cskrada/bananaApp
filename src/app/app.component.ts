4ed4bf32f5b34e16a664f8d5756addb7cbbd7db1
cskrada <frada@overcom.net>
2018-06-18 15:43:01 -0400

    fue agregado el condicional para que el menu sea visible a penas se loguee el usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -7,26 +7,30 @@ import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/Observable';
 
 
-// import { HomePage } from '../pages/home/home';
+import { HomePage } from '../pages/home/home';
+import { ClientsPage } from '../pages/clients/clients';
+
 import { LoginPage } from '../pages/login/login';
 @Component({
   templateUrl: 'app.html',
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = LoginPage;
   public is_logged : boolean;
   user: Observable<firebase.User>;
+  pages: Array<{title: string, component: any}>;
 
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public afAuth: AngularFireAuth) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-    this.is_logged = false;
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public afAuth: AngularFireAuth) {
+    this.initializeApp();
+
+    
     this.user = afAuth.authState;
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Clients', component: ClientsPage }
+    ];
 
       this.afAuth.authState.subscribe(res => {
           if (res && res.uid && this.is_logged == false) {
@@ -37,8 +41,23 @@ export class MyApp {
             this.is_logged = false;
           }
           });
+  }//fin de constructor
 
-
+initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+    this.is_logged = false;
   }
+  
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+
 }
 
