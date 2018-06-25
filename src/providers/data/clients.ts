86c2cf798e2fe3ed8ba4a08f81f0bf2403a50043
cskrada <frada@overcom.net>
2018-06-25 11:47:10 -0400

    se agrego el arreglo tipo objeto

--- src/providers/data/clients.ts
+++ src/providers/data/clients.ts
@@ -12,20 +12,113 @@ items: any;
 
 constructor(public http: Http) {
 
-	this.items = [
-		{title: 'one'},
-		{title: 'two'},
-		{title: 'three'},
-		{title: 'four'},
-		{title: 'five'},
-		{title: 'six'}
-	]
-}
+	this.initializeItems();
+
+
+} // fin del constructor
+
+	initializeItems(){
+		this.items = [
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Cocacola, CA',
+	    		address: 'boleita sur',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Granolay, CA',
+	    		address: 'Av intercomunal',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Doritos, CA',
+	    		address: 'caracas san andres',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'canal i',
+	    		address: 'av libertador',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'telesur',
+	    		address: 'boleita norte',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'flips',
+	    		address: 'locatel',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Farmatodo',
+	    		address: 'farmacia de farma',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'samsung',
+	    		address: 'los dos caminos',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Nokia',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	},
+	    	{
+	    		name: 'Pepsico, CA',
+	    		address: 'Av ppal las acacias',
+	    		state: 'Dtto. Capital',
+	    		phone: '04143198569',
+	    		email: 'csak@gmail.com'
+	    	}
+    	]; // fin de arreglo de objetos
+	} //fin InitialzeItems()
 
 	filterItems(searchTerm){
 		return this.items.filter((item) => {
-			return item.title.toLowerCase().indexOf(
+			return item.name.toLowerCase().indexOf(
 			searchTerm.toLowerCase()) > -1;
 		});
-   }	
+	}
+
+
 }
