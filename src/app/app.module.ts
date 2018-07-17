ace9f2b98d3b68b9428822c125e0e0711b23c6da
cskrada <frada@overcom.net>
2018-07-17 14:03:36 -0400

    eliminando espacios y comentarios en el codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,28 +29,9 @@ import { SignupPage } from '../pages/signup/signup';
 import { ResetpasswordPage } from '../pages/resetpassword/resetpassword';
 import { EmailPage } from '../pages/email/email';
 
-
 import { ChartsModule } from 'ng2-charts';
 import { DataProvider } from '../providers/data/data';
 import { ClientsProvider } from '../providers/data/clients';
-
-// conexion a bananaApp
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyBpIBL-h59BW8L3zHF22eHcMabZtkh4hMA",
-//   authDomain: "bananaapp8.firebaseapp.com",
-//   databaseURL: "https://bananaapp8.firebaseio.com",
-//   storageBucket: "bananaapp8.appspot.com",
-//   messagingSenderId: '623763613345'
-// };
-
-// bananaLight
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyCCc_m4cL7qTat3HRVwdgdPvAffdjSgp2I",
-//   authDomain: "banana-light.firebaseapp.com",
-//   databaseURL: "https://banana-light.firebaseio.com",
-//   storageBucket: "banana-light.appspot.com",
-//   messagingSenderId: '960529017626'
-// };
 
 @NgModule({
   declarations: [
--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -5,3 +5,21 @@ export const FIREBASE_CREDENTIALS = {
   storageBucket: "bananaapp8.appspot.com",
   messagingSenderId: '623763613345'
 };
+
+// conexion a bananaApp
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyBpIBL-h59BW8L3zHF22eHcMabZtkh4hMA",
+//   authDomain: "bananaapp8.firebaseapp.com",
+//   databaseURL: "https://bananaapp8.firebaseio.com",
+//   storageBucket: "bananaapp8.appspot.com",
+//   messagingSenderId: '623763613345'
+// };
+
+// bananaLight
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyCCc_m4cL7qTat3HRVwdgdPvAffdjSgp2I",
+//   authDomain: "banana-light.firebaseapp.com",
+//   databaseURL: "https://banana-light.firebaseio.com",
+//   storageBucket: "banana-light.appspot.com",
+//   messagingSenderId: '960529017626'
+// };
