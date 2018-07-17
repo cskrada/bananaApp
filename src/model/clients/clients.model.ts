958bcfcb7ca1845d799555de85788217cfb68532
cskrada <frada@overcom.net>
2018-07-17 14:49:32 -0400

    se creo un model de Clients para definir la estructura de clients (solo prueba)

---
+++ src/model/clients/clients.model.ts
@@ -0,0 +1,14 @@
+export interface Clients {
+	key?: string;
+	business_name: string;
+	tradename: string;
+	alias: string;
+	rif: string;
+	address: string;
+	region: string;
+	code_p: string;
+	city: string;
+	country: string;
+	phone: string;
+	email1: string;
+}
